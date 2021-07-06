26b2743f11e738cb2a7274f0dc53e93161411d05
Marco Magdy <marcomagdy241@gmail.com>
2021-07-06 11:54:40 +0200

    Modify S_DS_01

---
+++ Embedded Systems Level 1 BootCamp/S_DS_01/src/Balanced Parentheses/balancedParentheses.h
@@ -0,0 +1,10 @@
+#ifndef Balanced_Parentheses_H_
+#define Balanced_Parentheses_H_
+
+#include "../Stack/stack.h"
+
+#define Balance (uint8_t *) "Balanced"
+#define Not_Balance (uint8_t *) "Not Balanced"
+
+uint8_t *balancedParentheses(uint8_t *expression);
+#endif
