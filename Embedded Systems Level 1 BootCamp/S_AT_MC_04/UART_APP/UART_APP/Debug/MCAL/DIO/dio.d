39d8447f000d8fe5ad5d95b8673b44ca7265834c
Marco Magdy <marcomagdy241@gmail.com>
2021-07-14 21:14:12 +0200

    S_AT_MC_04 added successfully

---
+++ Embedded Systems Level 1 BootCamp/S_AT_MC_04/UART_APP/UART_APP/Debug/MCAL/DIO/dio.d
@@ -0,0 +1,9 @@
+MCAL/DIO/dio.d MCAL/DIO/dio.o: ../MCAL/DIO/dio.c ../MCAL/DIO/dio.h \
+ ../MCAL/DIO/../../Infrastructure/registers.h \
+ ../MCAL/DIO/../../Infrastructure/types.h
+
+../MCAL/DIO/dio.h:
+
+../MCAL/DIO/../../Infrastructure/registers.h:
+
+../MCAL/DIO/../../Infrastructure/types.h:
