5938f9570ebd55ae79de1f62132626c46ded7f1f
Marco Magdy <marcomagdy241@gmail.com>
2021-07-28 01:44:59 +0200

    S_AT_MC_08 added successfully

---
+++ Embedded Systems Level 1 BootCamp/S_AT_MC_08/I2C Master/I2C Master/main.c
@@ -0,0 +1,18 @@
+/*
+ * I2C Master.c
+ *
+ * Created: 7/28/2021 12:59:13 AM
+ * Author : Marco
+ */ 
+
+#include "APP/I2C_MASTER/master.h"
+
+int main(void)
+{
+    I2C_Master_Init();
+	 I2C_Master_Update();
+    while (1) 
+    {
+    }
+}
+
