f973023c69979a9fcca4c4b2b3cbb80b655e3930
Marco Magdy <marcomagdy241@gmail.com>
2021-08-01 23:06:55 +0200

    Change receive string function

--- Embedded Systems Level 1 BootCamp/S_AT_MC_04/UART_APP/UART_APP/main.c
+++ Embedded Systems Level 1 BootCamp/S_AT_MC_04/UART_APP/UART_APP/main.c
@@ -21,28 +21,23 @@ int main(void)
 		UART_TRANSMITER_INT_DISABLE,
 		UART_RECIEVER_INT_DISABLE
 	};
-	ST_DIO_config port_b_5 = {DIO_PORTB, PIN_5, OUTPUT};
-	DIO_Init(&port_b_5);
 	UART_Init(configs);
 	UART_TransmitString((uint8_t *) "Start");
-	UART_TransmitChar('\r');
+	UART_TransmitChar(NEW_LINE);
 	uint16_t received ;
 	uint8_t y[10];
-	UART_TransmitString((uint8_t *) "Received: ");
-	if (!(UART_RecieveString(y)))
-	{
-		DIO_Write_PIN(DIO_PORTB, PIN_5, HIGH);
-	}
-	UART_TransmitString((uint8_t *) "Sent: ");
+	UART_TransmitString((uint8_t *) "Received String: ");
+	UART_RecieveString(y);
+	UART_TransmitString((uint8_t *) "Sent String: ");
 	UART_TransmitString(y);
 	UART_TransmitChar(NEW_LINE);
 	/* Replace with your application code */
 	while (1)
 	{
-		UART_TransmitString((uint8_t *) "Received: ");
+		UART_TransmitString((uint8_t *) "Received Char: ");
 		UART_RecieveChar(&received);
 		UART_TransmitString((uint8_t *) "       ");
-		UART_TransmitString((uint8_t *) "Sent: ");
+		UART_TransmitString((uint8_t *) "Sent Char: ");
 		UART_TransmitChar(received);
 		UART_TransmitChar(NEW_LINE);
 	}
